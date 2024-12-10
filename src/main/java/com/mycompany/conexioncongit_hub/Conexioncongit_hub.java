adf9befabf9906bc211106b1c9fc3d9606b3252f
jesusjordan2426 <jesus.jordan.vargas@colexio-karbo.com>
2024-12-10 21:51:36 +0100

    prueba de conexion 2.0

--- src/main/java/com/mycompany/conexioncongit_hub/Conexioncongit_hub.java
+++ src/main/java/com/mycompany/conexioncongit_hub/Conexioncongit_hub.java
@@ -12,5 +12,15 @@ public class Conexioncongit_hub {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+
+//ESTO ES UNA MODIFICACIÓN CUALQUIERA PARA LA PRUEBA DE ACTUALIZACION EN GITHUB.
+
+
+        System.out.println("Hola mundo1");
+
+        System.out.println("Hola mundo2");
+
+        System.out.println("Hola mundo3");
     }
 }
