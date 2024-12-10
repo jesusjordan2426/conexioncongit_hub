ff8a99874a03c695050c898773d6349bd3d7627e
jesusjordan2426 <jesus.jordan.vargas@colexio-karbo.com>
2024-12-10 21:27:49 +0100

    prueba de conexion 1.0

---
+++ src/main/java/com/mycompany/conexioncongit_hub/Conexioncongit_hub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conexioncongit_hub;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Conexioncongit_hub {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
